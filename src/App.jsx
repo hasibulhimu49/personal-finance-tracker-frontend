52eed5b0576348f021375f51cc2421982862bddb
Mohammad Hasibul Hasan <mohammadhasibulhasanpga4149@gmail.com>
2026-05-29 22:52:50 +0600

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Navbar from "./components/layout/Navbar";
 import Login from "./pages/auth/Login";
 import Registration from "./pages/auth/Registration"
 
 const App=()=>{
   return(
     <div>
-      <Registration/>
-      <Login/>
+      {/* <Registration/>
+      <Login/> */}
+      <Navbar/>
     </div>
   )
 }
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="h-10 w-full bg-amber-100 flex justify-end gap-10 items-center pr-10">
+      <a href="#">Home</a>
+      <a href="#">Home</a>
+      <a href="#">Home</a>
+      <a href="#">Home</a>
+      <a href="#">Home</a>
+    </div>
+  );
+};
+
+export default Navbar;
