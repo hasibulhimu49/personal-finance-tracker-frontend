92db4aed5cb481b531244b59ecf3842bee7332f2
Mohammad Hasibul Hasan <mohammadhasibulhasanpga4149@gmail.com>
2026-05-30 23:30:15 +0600

    dashboard

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+const Dashboard=()=>{
+    return(
+        <div>
+            <h1>PerFin</h1>
+            <a href="#">Overview</a>
+            <a href="#">Budget</a>
+            <a href="#">Expense</a>
+        </div>
+    )
+}
